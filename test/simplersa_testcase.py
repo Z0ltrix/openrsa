4f3a715d83699e88011d030b845faa128c435c9a
Christian Pfarr <pfarrch@gmail.com>
2017-09-13 15:55:30 +0200

    writing unittests for simplersa for signing and verification

--- test/simplersa_testcase.py
+++ test/simplersa_testcase.py
@@ -16,6 +16,12 @@ class SimpleRsaTestCase(unittest.TestCase):
         self.deciphered_data = self.simple_rsa.decipher(self.enciphered_data,
                                                         self.private_key.get_decipher_exponent(),
                                                         self.public_key.get_modulo())
+        self.signed_data = self.simple_rsa.sign(self.data,
+                                                self.private_key.get_decipher_exponent(),
+                                                self.private_key.get_modulo())
+        self.verified_data = self.simple_rsa.verify(self.signed_data,
+                                                    self.public_key.get_encipher_exponent(),
+                                                    self.public_key.get_modulo())
 
     def tearDown(self):
         del self.simple_rsa
@@ -25,6 +31,11 @@ class SimpleRsaTestCase(unittest.TestCase):
         del self.public_key
         del self.enciphered_data
         del self.deciphered_data
+        del self.signed_data
+        del self.verified_data
 
-    def test_simple_rsa(self):
+    def test_decipher(self):
         self.assertTrue(self.data == self.deciphered_data, "Decryption doesnt work")
+
+    def test_verify(self):
+        self.assertTrue(self.data == self.verified_data, "Verification doesnt work")
